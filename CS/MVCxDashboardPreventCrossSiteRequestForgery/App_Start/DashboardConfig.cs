f5fa5433103a90e928290be6b6e14622bc873eb6
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2021-12-27 12:07:40 +0300

    Remove unused datasources

--- CS/MVCxDashboardPreventCrossSiteRequestForgery/App_Start/DashboardConfig.cs
+++ CS/MVCxDashboardPreventCrossSiteRequestForgery/App_Start/DashboardConfig.cs
@@ -27,25 +27,8 @@ namespace MVCxDashboardPreventCrossSiteRequestForgery {
                 .Build("Sales Person");
             sqlDataSource.Queries.Add(query);
             dataSourceStorage.RegisterDataSource("sqlDataSource", sqlDataSource.SaveToXml());
-            
-            // Registers an Object data source.
-            DashboardObjectDataSource objDataSource = new DashboardObjectDataSource("Object Data Source");
-            dataSourceStorage.RegisterDataSource("objDataSource", objDataSource.SaveToXml());
-            
-            // Registers an Excel data source.
-            DashboardExcelDataSource excelDataSource = new DashboardExcelDataSource("Excel Data Source");
-            excelDataSource.FileName = HostingEnvironment.MapPath(@"~/App_Data/Sales.xlsx");
-            excelDataSource.SourceOptions = new ExcelSourceOptions(new ExcelWorksheetSettings("Sheet1"));
-            dataSourceStorage.RegisterDataSource("excelDataSource", excelDataSource.SaveToXml());
-            
-            DashboardConfigurator.Default.SetDataSourceStorage(dataSourceStorage);
-            DashboardConfigurator.Default.DataLoading += DataLoading;
-        }
 
-        private static void DataLoading(object sender, DataLoadingWebEventArgs e) {
-            if(e.DataSourceName == "Object Data Source") {
-                e.Data = Invoices.CreateData();
-            }
+            DashboardConfigurator.Default.SetDataSourceStorage(dataSourceStorage);
         }
     }
 }
